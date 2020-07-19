9d3dbb072d12428fccc4709d7e0ce794fc8bec8c
fiwon123 <felipeinoue@yahoo.com.br>
2020-07-19 15:42:19 -0300

    all

---
+++ Assets/Throw.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+    public GameObject spherePrefab;
+    public GameObject cubePrefab;
+    public Material green;
+    public Material red;
+
+    Perceptron p;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        p = GetComponent<Perceptron>();
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetKeyDown("1")){
+            GameObject g = Instantiate(spherePrefab, Camera.main.transform.position, Camera.main.transform.rotation);
+            g.GetComponent<Renderer>().material = red;
+            g.GetComponent<Rigidbody>().AddForce(0, 0, 500);
+            p.SendInput(0, 0, 0);
+        }
+        else if (Input.GetKeyDown("2")){
+            GameObject g = Instantiate(spherePrefab, Camera.main.transform.position, Camera.main.transform.rotation);
+            g.GetComponent<Renderer>().material = green;
+            g.GetComponent<Rigidbody>().AddForce(0, 0, 500);
+            p.SendInput(0, 1, 1);
+        }
+        else if (Input.GetKeyDown("3")){
+            GameObject g = Instantiate(cubePrefab, Camera.main.transform.position, Camera.main.transform.rotation);
+            g.GetComponent<Renderer>().material = red;
+            g.GetComponent<Rigidbody>().AddForce(0, 0, 500);
+            p.SendInput(1, 0, 1);
+        }
+        else if (Input.GetKeyDown("4")){
+            GameObject g = Instantiate(cubePrefab, Camera.main.transform.position, Camera.main.transform.rotation);
+            g.GetComponent<Renderer>().material = green;
+            g.GetComponent<Rigidbody>().AddForce(0, 0, 500);
+            p.SendInput(1, 1, 1);
+        }
+    }
+}
